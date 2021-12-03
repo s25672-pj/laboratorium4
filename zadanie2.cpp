bd805d0ebca8c54ead5d3b96daf9e2864dc8fc06
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2021-12-03 17:02:26 +0100

    Add files via upload

---
+++ zadanie2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int liczba, L, W, maxL, maxW;
+    int tab[10];
+
+    
+    cout << "wprowadz 10 liczb" << endl;
+
+    for (int i = 0;i <= 9;i++) {
+        cin >> liczba;
+        tab[i] = liczba;
+    }
+
+    cout << endl;
+    maxL = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        W = tab[i]; L = 0;
+        for (int j = 0; j < 9; j++) if (tab[j] == W) L++;
+        if (L > maxL)
+        {
+            maxL = L; maxW = W;
+        }
+    }
+
+    
+
+    cout << endl <<"Najczesciej wystepujaca wartoscia jest: " << maxW << " i wystepuje " << maxL << " razy" << endl << endl;
+    return 0;
+}
+
